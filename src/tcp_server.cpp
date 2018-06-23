c309a2c5e9f4afb7ec2140e0f075810dc50658ee
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-06-23 10:42:26 +0800

    update tcp_server

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/signal.h>
 
 #include <cstring>         
 #include <cstdlib> 
@@ -13,6 +14,7 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <atomic>
 
 
 #include "tcp_server.hpp"
@@ -20,6 +22,19 @@
 
 
 namespace mongols {
+
+    bool tcp_server::done = false;
+
+    void tcp_server::signal_normal_cb(int sig) {
+        switch (sig) {
+            case SIGTERM:
+            case SIGHUP:
+            case SIGQUIT:
+            case SIGINT:
+                tcp_server::done = true;
+                break;
+        }
+    }
 
     tcp_server::tcp_server(const std::string& host
             , int port
@@ -29,39 +44,31 @@ namespace mongols {
             , int max_event_size) :
     epoll(max_event_size, timeout)
     , host(host), port(port), listenfd(0), serveraddr()
-    , buffer_size(buffer_size), thread_size(thread_size) {
+    , buffer_size(buffer_size), th_pool(thread_size) {
 
     }
 
     tcp_server::~tcp_server() {
-        for (;;) {
-            int v;
-            if (this->q.try_pop(v)) {
-                this->epoll.del(v);
-                close(v);
-            } else {
-                break;
-            }
-        }
-        if (this->listenfd > 0) {
-            close(this->listenfd);
-        }
+
     }
 
     void tcp_server::run(const std::function<std::pair<std::string, bool>(const std::string&) >& g) {
 
         int connfd;
-        struct timeval timeout;
-
 
         this->listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
         int on = 1;
-        setsockopt(this->listenfd, SOL_SOCKET, SO_REUSEPORT, (const char *) &on, sizeof (on));
-        timeout.tv_sec = this->epoll.expires() < 0 ? 5 : this->epoll.expires() / 1000;
-        timeout.tv_usec = 0;
-        setsockopt(this->listenfd, SOL_SOCKET, SO_SNDTIMEO, (const char *) &timeout, sizeof (timeout));
-        setsockopt(this->listenfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &timeout, sizeof (timeout));
+        setsockopt(this->listenfd, SOL_SOCKET, SO_REUSEPORT, &on, sizeof (on));
+
+        struct timeval send_timeout, recv_timeout;
+        send_timeout.tv_sec = this->epoll.expires() < 0 ? 5 : this->epoll.expires() / 1000;
+        send_timeout.tv_usec = 0;
+        setsockopt(this->listenfd, SOL_SOCKET, SO_SNDTIMEO, &send_timeout, sizeof (send_timeout));
+
+        recv_timeout.tv_sec = this->epoll.expires() < 0 ? 5 : this->epoll.expires() / 1000;
+        recv_timeout.tv_usec = 0;
+        setsockopt(this->listenfd, SOL_SOCKET, SO_RCVTIMEO, &recv_timeout, sizeof (recv_timeout));
 
 
 
@@ -98,19 +105,6 @@ ev_error:
             }
         };
 
-        std::vector<std::thread> th;
-        for (size_t i = 0; i < this->thread_size; ++i) {
-            th.push_back(std::move(std::thread(
-                    [&]() {
-                        while (1) {
-                            int fd;
-                            this->q.wait_and_pop(fd);
-                            w(fd);
-                            std::this_thread::yield();
-                        }
-                    }
-            )));
-        }
 
         std::function<void(struct epoll_event*) > f = [&](struct epoll_event * event) {
             if ((event->events & EPOLLERR) ||
@@ -120,22 +114,25 @@ ev_error:
             } else if (event->events & EPOLLRDHUP) {
                 close(event->data.fd);
             } else if (event->data.fd == this->listenfd) {
-                while (1) {
+                while (!tcp_server::done) {
                     struct sockaddr_in clientaddr;
                     socklen_t clilen;
                     connfd = accept(listenfd, (struct sockaddr*) &clientaddr, &clilen);
                     if (connfd > 0) {
-                        this->epoll.add(connfd, EPOLLIN | EPOLLRDHUP | EPOLLET);
                         setnonblocking(connfd);
                         this->epoll.add(connfd, EPOLLIN | EPOLLRDHUP | EPOLLET);
 
+                        if (this->th_pool.size() > 0) {
+                            std::this_thread::yield();
+                        }
                     } else {
                         break;
                     }
                 }
             } else {
-                if (this->thread_size > 0) {
-                    this->q.push(event->data.fd);
+                if (this->th_pool.size() > 0) {
+                    this->th_pool.submit(std::bind(w, int(event->data.fd)));
+                    std::this_thread::yield();
                 } else {
                     w(event->data.fd);
                 }
@@ -143,18 +140,14 @@ ev_error:
         };
 
 
+        signal(SIGHUP, tcp_server::signal_normal_cb);
+        signal(SIGTERM, tcp_server::signal_normal_cb);
+        signal(SIGINT, tcp_server::signal_normal_cb);
+        signal(SIGQUIT, tcp_server::signal_normal_cb);
 
-
-        while (1) {
-            int ret = this->epoll.wait();
-            this->epoll.loop(ret, f);
+        while (!tcp_server::done) {
+            this->epoll.loop(f);
         }
-
-
-        for (auto& i : th) {
-            i.join();
-        }
-
 
     }
 
