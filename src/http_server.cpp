9ae87cde0683dfe290b8df053ea5f835fb8a6750
centos <centos@localhost>
2018-07-24 04:34:47 +0800

    update readme

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -156,7 +156,7 @@ namespace mongols {
     std::string http_server::create_response(mongols::response& res, bool b) {
         std::string output;
         output.append("HTTP/1.1 ").append(std::to_string(res.status)).append(" " + this->get_status_text(res.status)).append("\r\n");
-        res.headers.insert(std::make_pair("Server", mongols_http_server_version));
+        res.headers.insert(std::move(std::make_pair("Server", mongols_http_server_version)));
         if (b == KEEPALIVE_CONNECTION) {
             res.headers.insert(std::move(std::make_pair("Connection", "keep-alive")));
         } else {
